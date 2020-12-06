8305311d540e6c4557c36df23d6e93b0c4f62bf4
Nikolay Karaivanov <nikolay80@mail.bg>
2020-12-06 16:13:16 +0200

    Add files via upload

---
+++ RadtoDeg/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RadtoDeg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rad = double.Parse(Console.ReadLine());
+            double deg = Math.Round(rad * (180 / Math.PI));           
+            Console.WriteLine(deg);
+        }
+    }
+}
