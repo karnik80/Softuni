ae2c2cb0531c02f94f2e3d4a5017d98a58855846
Nikolay Karaivanov <nikolay80@mail.bg>
2020-12-06 16:14:11 +0200

    Add files via upload

---
+++ Projects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Please enter architect's name : ");
+            string name = Console.ReadLine();
+            //Console.Write("Please enter the number of projects (from 1 to 100) : ");
+            int numProjects = int.Parse(Console.ReadLine());
+
+            int hours = numProjects * 3;
+
+            Console.WriteLine($"The architect {name} will need {hours} hours to complete {numProjects} project/s.");
+        }
+    }
+}
