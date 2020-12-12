3bb754a5ab01aebe3c4a7981dd8751564e515a26
Nikolay Karaivanov <nikolay80@mail.bg>
2020-12-12 16:06:27 +0200

    Add files via upload

---
+++ Nsum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nsum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                sum += x;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
