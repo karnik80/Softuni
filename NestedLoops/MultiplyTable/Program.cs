1c717d76a27a7d1f028b2ebe0c52bee4eeca0023
Nikolay Karaivanov <nikolay80@mail.bg>
2020-12-17 08:59:43 +0200

    Add files via upload

---
+++ NestedLoops/MultiplyTable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiplyTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiply1 = 0;
+            int multiply2 = 0;
+            int result;
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i*j}");
+                }
+            }
+        }
+    }
+}
