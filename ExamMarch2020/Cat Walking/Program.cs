c3f3d6cd1ea67a38b6e8727a10a42077c3c01d11
Nikolay Karaivanov <nikolay80@mail.bg>
2021-01-30 10:45:16 +0100

    Add files via upload

---
+++ ExamMarch2020/Cat Walking/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cat_Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesPerWalk = int.Parse(Console.ReadLine());
+            int walksPerDay = int.Parse(Console.ReadLine());
+            int caloriesPerDay = int.Parse(Console.ReadLine());
+
+            int totalMinutes = minutesPerWalk * walksPerDay;
+            int burnedCalories = totalMinutes * 5;
+
+            if((caloriesPerDay/2) <= burnedCalories)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {burnedCalories}.");
+            }
+            else if((caloriesPerDay / 2) > burnedCalories)
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {burnedCalories}.");
+            }
+        }
+    }
+}
