95299b57e0d4041621b6659f6a667bc4f14142d7
Nikolay Karaivanov <nikolay80@mail.bg>
2021-02-07 16:59:37 +0100

    Add files via upload

---
+++ ConditionalStatements/Add15min/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Add15min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hourIn = int.Parse(Console.ReadLine());
+            int minutesIn = int.Parse(Console.ReadLine());
+
+            int minutes = 0;
+            int hour = 0;
+
+            minutes = minutesIn + 15;
+            hour = hourIn;
+
+            if(minutes >= 60)
+            {
+                hour++;
+                minutes = minutes - 60;
+            }
+
+            if(hour >= 24)
+            {
+                hour = 0;
+            }
+
+            if(minutes < 10)
+            {
+                Console.WriteLine($"{hour}:0{minutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{hour}:{minutes}");
+            }
+
+        }
+    }
+}
