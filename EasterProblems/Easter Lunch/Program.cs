e55df7399b230ac86fd1edff1ddd58c92c55e7d7
Nikolay Karaivanov <nikolay80@mail.bg>
2021-02-20 17:33:32 +0100

    Add files via upload

---
+++ EasterProblems/Easter Lunch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Easter_Lunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cozunak = int.Parse(Console.ReadLine());
+            int eags = int.Parse(Console.ReadLine());
+            int cookies = int.Parse(Console.ReadLine());
+
+            double budget = (cozunak * 3.20) + (eags * 4.35) + (cookies * 5.40) + (eags * 12 * 0.15);
+            Console.WriteLine($"{budget:F2}");
+        }
+    }
+}
