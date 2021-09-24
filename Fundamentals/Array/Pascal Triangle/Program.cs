e96fda27e518583e36cdd597cd052fc64c4eccda
Nikolay Karaivanov <nikolay80@mail.bg>
2021-09-25 02:41:02 +0300

    Add files via upload

---
+++ Fundamentals/Array/Pascal Triangle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            int[] main = new int[n];
+            main[0] = 1;
+            int count = 1;
+            Console.WriteLine(main[0]);
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int[] temp = new int[i + 1 + 1];
+
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    if (j == 0)
+                    {
+                        temp[j] = main[0];
+                    }
+                    else if (j == temp.Length - 1)
+                    {
+                        temp[j] = main[count - 1];
+                    }
+                    else
+                    {
+                        temp[j] = main[j - 1] + main[j];
+                    }
+                }
+                //Copy temp to Main
+                for (int k = 0; k < temp.Length; k++)
+                {
+                    main[k] = temp[k];
+                }
+                count = temp.Length;
+                Console.WriteLine(string.Join(" ", temp));
+            }
+        }
+    }
+}
