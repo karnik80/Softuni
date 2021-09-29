80ea961c43c8f7e078a0e3e4e1ed2121a8b5d848
Nikolay Karaivanov <nikolay80@mail.bg>
2021-09-29 15:07:57 +0300

    Add files via upload

---
+++ Fundamentals/Array/Top Integers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                bool isItBigger = true;
+
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[i] <= input[j])
+                    {
+                        isItBigger = false;
+                    }
+                }
+
+                if (isItBigger)
+                {
+                    Console.Write($"{input[i]} ");
+                }
+            }
+        }
+    }
+}
---
+++ Fundamentals/Array/Zig-Zag Arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr1 = new int[n];
+            int[] arr2 = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] temp = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                if (i % 2 == 0)
+                {
+                    arr1[i] = temp[1];
+                    arr2[i] = temp[0];
+                }
+                else
+                {
+                    arr1[i] = temp[0];
+                    arr2[i] = temp[1];
+                }
+            }
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.Write($"{arr2[i]} ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.Write($"{arr1[i]} ");
+            }
+        }
+    }
+}
