9b2249a1c8d87585196f85deb8eff84c65b3df5c
Nikolay Karaivanov <nikolay80@mail.bg>
2021-09-29 15:12:59 +0300

    Add files via upload

---
+++ Fundamentals/Associative_Arrays/A Miner Task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, uint> myTask = new Dictionary<string, uint>();
+
+            string key = string.Empty;
+
+            while((key = Console.ReadLine()) != "stop")
+            {
+                uint val = uint.Parse(Console.ReadLine());
+
+                if (myTask.ContainsKey(key))
+                {
+                    myTask[key] += val;
+                }
+                else
+                {
+                    myTask.Add(key, val);
+                }
+            }
+
+            foreach (var item in myTask)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
